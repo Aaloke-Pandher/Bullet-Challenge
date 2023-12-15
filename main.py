4e2547abcae0d2eb00689d7875d6f48c914173c7
Aaloke-Pandher <98190131+Aaloke-Pandher@users.noreply.github.com>
2023-12-15 13:29:56 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,97 @@
+# Bullet Challenge 
+import pygame 
+import random
+ 
+# Define colors 
+Grey = [128, 128, 128] 
+White = [255, 255, 255] 
+Blue = [0, 0, 255]  
+Red = [255, 0, 0] 
+Green = [0, 255, 0]  
+Black = [0, 0, 0] 
+randColor = [random.randrange(0, 255), random.randrange(0, 255), random.randrange(0, 255)]
+
+# Player Class 
+class Player():
+    def __init__(self, x, y, w, h):
+        self.x = x 
+        self.y = y 
+        self.w = w 
+        self.h = h  
+    def drawPlayer(self, screen): 
+        pygame.draw.ellipse(screen, White, [self.x, self.y, self.w, self.h]) 
+
+# Circle Class 
+class Circle():
+    def __init__(self, x, y, w, h, c):  
+        self.h = h
+        self.w = w 
+        self.x = x 
+        self.y = y   
+        self.xspeed = random.randrange(-3, 3)
+        self.yspeed = random.randrange(-3, 3) 
+        self.c = c
+    def drawCircle(self, screen):
+        pygame.draw.ellipse(screen, self.c, [self.x, self.y, self.w, self.h], 2)  
+    def moveCircle(self):
+        self.x += self.xspeed 
+        self.y += self.yspeed  
+
+# List of Circles 
+circles = [] 
+for i in range(15): 
+    randColor = [random.randrange(0, 255), random.randrange(0, 255), random.randrange(0, 255)]
+    wHRan = random.randrange(30, 40)
+    circles.append(Circle(random.randrange(0, 770), random.randrange(0, 400), wHRan, wHRan, randColor))       
+
+# Wall Collision 
+def wallCollision(rect): 
+    if rect.x >= 800 - rect.w:
+        rect.xspeed = -rect.xspeed
+        rect.yspeed = rect.yspeed  
+    elif rect.x <= 0:
+        rect.xspeed = -rect.xspeed
+        rect.yspeed = rect.yspeed
+    elif rect.y >= 450 - rect.h:
+        rect.yspeed = -rect.yspeed
+        rect.xspeed = rect.xspeed  
+    elif rect.y <= 0:
+        rect.yspeed = -rect.yspeed 
+        rect.xspeed = rect.xspeed  
+
+player = Player(375, 550, 50, 50)
+# Main function 
+def main():
+    pygame.init() 
+    # Canvas
+    size = (800, 600)
+    screen = pygame.display.set_mode(size)  
+    # Loop until the user clicks the close button.
+    done = False
+ 
+    # Used to manage how fast the screen updates
+    clock = pygame.time.Clock()
+ 
+    # Main Program Loop 
+    while not done:
+        # Main event loop
+        for event in pygame.event.get(): 
+            if event.type == pygame.QUIT: 
+                done = True  
+        
+        screen.fill(Black)
+        pygame.draw.rect(screen, White, [0, 450, 800, 15]) 
+        player.drawPlayer(screen)  
+        for i in range(len(circles)):
+            circles[i].drawCircle(screen)  
+            wallCollision(circles[i])
+            circles[i].moveCircle()
+        pygame.display.flip()
+ 
+        # --- Limit frames
+        clock.tick(60)  
+
+     # Close window
+    pygame.quit()  
+
+main()
